9f635f56a1f36bc706a1c501d0546a7870b52d76
Gerónimo Pellicer <geropellicer@gmail.com>
2023-12-15 13:15:01 -0300

    Added  dataset por cliente

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='empresa4',
     packages=find_packages(include=['empresa4', 'empresa4.*']),
-    version='2.0.1',
+    version='2.0.2',
     description='Empresa 4 Library for Laboratorio de Implementación 3',
     author='Gero',
     author_email='geropellicer@gmail.com',  # Type in your E-Mail
